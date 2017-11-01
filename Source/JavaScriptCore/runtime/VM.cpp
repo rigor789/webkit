af091a871bee6d023a738d2dfc46da9ab2272adb
Deyan Ginev <deyan.ginev@progress.com>
2017-11-01 14:44:30 +0200

    fix missing stack trace info in recreated Exception instances

--- Source/JavaScriptCore/runtime/VM.cpp
+++ Source/JavaScriptCore/runtime/VM.cpp
@@ -602,8 +602,22 @@ void VM::throwException(ExecState* exec, Exception* exception)
 JSValue VM::throwException(ExecState* exec, JSValue thrownValue)
 {
     Exception* exception = jsDynamicCast<Exception*>(thrownValue);
-    if (!exception)
-        exception = Exception::create(*this, thrownValue);
+    if (!exception) {
+        // protect against throw null; in JavaScript
+        if (thrownValue.getObject() != nullptr) {
+            // Exception::create stores the Exception instance in the passed thrownValue. If we have an Exception instance here we reuse it instead of
+            // recreating it. In this way we keep the stack trace at the point where the original exception occured.
+            JSValue nsException = thrownValue.getObject()->getDirect(*this, Identifier::fromString(this, Exception::NS_EXCEPTION_IDENTIFIER_STRING));
+        
+            if (!nsException.isUndefinedOrNull() && nsException.isCell() && nsException.asCell() != nullptr ) {
+                exception = jsDynamicCast<Exception*>(nsException);
+            }
+        }
+        
+        if (!exception) {
+            exception = Exception::create(*this, thrownValue);
+        }
+    }
 
     throwException(exec, exception);
     return JSValue(exception);
